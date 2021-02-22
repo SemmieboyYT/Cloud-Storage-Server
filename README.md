454a9aadb3b9ef351dc559a5f16ea7d803a83027
Semmieboy YT <58337334+SemmieboyYT@users.noreply.github.com>
2021-02-22 18:43:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cloud Storage Server
+ A simple HTTP server where you can store, view and download files.
---
+++ src/semmieboy_yt/cloud_storage_server/Main.java
@@ -0,0 +1,7 @@
+package semmieboy_yt.cloud_storage_server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
